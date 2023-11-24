742343cfb8e81a05e189869c7d7071e1121e9368
unknown <joaochecco@gmail.com>
2023-11-24 13:50:51 -0300

    refatoração do DTO

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -34,7 +34,7 @@ public class PagamentoController {
     @PostMapping
     public ResponseEntity<PagamentoDto> cadastrar(@RequestBody @Valid PagamentoDto dto, UriComponentsBuilder uriBuilder) {
         PagamentoDto pagamento = service.criarPagamento(dto);
-        URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.id()).toUri();
+        URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
 
         return ResponseEntity.created(endereco).body(pagamento);
     }
--- src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
+++ src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -1,38 +1,21 @@
 package br.com.alurafood.pagamentos.dto;
 
 import br.com.alurafood.pagamentos.model.Status;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
-public record PagamentoDto(
-        @NotNull
-        Long id,
-        @NotNull
-        @Positive
-        BigDecimal valor,
-        @NotBlank
-        @Size(max = 100)
-        String nome,
-        @NotBlank
-        @Size(max = 19)
-        String numero,
-        @NotBlank
-        @Size(max = 7)
-        String expiracao,
-        @NotBlank
-        @Size(min = 3, max = 3)
-        String codigo,
-        @NotNull
-        Status status,
-        @NotNull
-        Long pedidoId,
-        @NotNull
-        Long formaDePagamentoId
-) {
+@Getter
+@Setter
+public class PagamentoDto {
+        private Long id;
+        private BigDecimal valor;
+        private String nome;
+        private String numero;
+        private String expiracao;
+        private String codigo;
+        private Status status;
+        private Long pedidoId;
+        private Long formaDePagamentoId;
 }
